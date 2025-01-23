43da9e5a846c061eb1145cdd261b8f265d14571f
Catalina Popa <catalina.popa@fundmore.ai>
2025-01-23 19:30:01 +0200

    remove api key

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,10 +1,11 @@
-const API_KEY = "X4o5beg0SOUQ6Qu9R5mM6BXLVEeT61no";
+// const API_KEY = "X4o5beg0SOUQ6Qu9R5mM6BXLVEeT61no";
+import config from "../config";
 const LOCATION_KEY = 287713;
 const BASE_URL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${LOCATION_KEY}`;
 
 export const fetchWeatherForecast = async () => {
   try {
-    const response = await fetch(`${BASE_URL}?apikey=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}?apikey=${config.API_KEY}`);
     if (!response.ok) {
       throw new Error("Failed to fetch weather data");
     }
@@ -16,7 +17,7 @@ export const fetchWeatherForecast = async () => {
         max: forecast.Temperature.Maximum.Value,
       },
       hasPrecipitation: forecast.Day.HasPrecipitation,
-      precipitationType: forecast.Day.PrecipitationType || "Nooone",
+      precipitationType: forecast.Day.PrecipitationType || "None",
     }));
   } catch (error) {
     console.error("Error fetching weather data:", error);
